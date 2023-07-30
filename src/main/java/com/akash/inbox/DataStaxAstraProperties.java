8598b387ebeae09bf67a64c27cd5c7b40852e5ad
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-30 14:51:47 +0530

    [refactor] package name

--- src/main/java/com/akash/inbox/DataStaxAstraProperties.java
+++ src/main/java/com/akash/inbox/DataStaxAstraProperties.java
@@ -1,11 +1,14 @@
-package io.javabrains;
+package com.akash.inbox;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
+@Configuration
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
+
     private File secureConnectBundle;
 
     public File getSecureConnectBundle() {
--- src/main/java/com/akash/inbox/GitInboxApp.java
+++ src/main/java/com/akash/inbox/GitInboxApp.java
@@ -1,18 +1,16 @@
-package io.javabrains;
+package com.akash.inbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 import java.nio.file.Path;
 
 @SpringBootApplication
+//@EnableConfigurationProperties(DataStaxAstraProperties.class)
 @RestController
 public class GitInboxApp {
 
@@ -30,6 +28,5 @@ public class GitInboxApp {
 		Path bundle=astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
 	}
-	
 
 }
