b3c83299489ab024cbc7cc2d93ef08c537422136
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-30 11:25:28 +0530

    connect astra db

--- src/main/java/io/javabrains/GitInboxApp.java
+++ src/main/java/io/javabrains/GitInboxApp.java
@@ -2,11 +2,15 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -20,6 +24,12 @@ public class GitInboxApp {
 //	public Mono<String> user(@AuthenticationPrincipal Mono<OAuth2User> principal) {
 //		return principal.map(val -> val.getAttribute("login"));
 //	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle=astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 	
 
 }
