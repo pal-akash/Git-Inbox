fc86bf80e02d5468458ee0105edbb6a878333e80
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-31 01:04:33 +0530

    create inbox controller

---
+++ src/main/java/com/akash/inbox/controller/InboxController.java
@@ -0,0 +1,34 @@
+package com.akash.inbox.controller;
+
+import com.akash.inbox.folders.Folder;
+import com.akash.inbox.folders.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class InboxController {
+
+    @Autowired
+    private FolderRepository folderRepository;
+
+    @GetMapping(value = "/")
+    public String homePage(@AuthenticationPrincipal OAuth2User principal, Model model){
+
+        if(principal==null || !StringUtils.hasText(principal.getAttribute("login"))){
+            return "index";
+        }
+
+        String userId = principal.getAttribute("login");
+        List<Folder> userFolders = folderRepository.findAllById(userId);
+        model.addAttribute("userFolders", userFolders);
+
+        return "inbox-page";
+    }
+}
--- src/main/java/com/akash/inbox/folders/FolderRepository.java
+++ src/main/java/com/akash/inbox/folders/FolderRepository.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FolderRepository extends CassandraRepository<Folder, String> {
 
+    List<Folder> findAllById(String id);
 }
