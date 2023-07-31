0dc65c9260ca1856293baffcede23c33591d8586
Akash Pal <108969268+pal-akash@users.noreply.github.com>
2023-07-31 22:34:56 +0530

    added folder view to inbox page

--- src/main/java/com/akash/inbox/GitInboxApp.java
+++ src/main/java/com/akash/inbox/GitInboxApp.java
@@ -1,18 +1,27 @@
 package com.akash.inbox;
 
+import com.akash.inbox.folders.Folder;
+import com.akash.inbox.folders.FolderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+import javax.annotation.PostConstruct;
 import java.nio.file.Path;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(DataStaxAstraProperties.class)
 @RestController
 public class GitInboxApp {
+
+	@Autowired
+	FolderRepository folderRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitInboxApp.class, args);
@@ -27,6 +36,13 @@ public class GitInboxApp {
 	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
 		Path bundle=astraProperties.getSecureConnectBundle().toPath();
 		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void init(){
+		folderRepository.save(new Folder("pal-akash", "Inbox", "blue"));
+		folderRepository.save(new Folder("pal-akash", "Sent", "green"));
+		folderRepository.save(new Folder("pal-akash", "Important", "yellow"));
 	}
 
 }
